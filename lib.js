fea8824a2aa96c3d894cc02d77289eab4f31d023
Rosalind Skelton <rosskelton@rosmac.local>
2016-04-02 15:45:04 +0200

    changes

--- lib.js
+++ lib.js
@@ -1,22 +1,22 @@
 var container = d3.select("#container")
 var svg_element = container[0][0];
+var scen = '16'
 
 function display_overlay(dam_name, level) {
     var level = parseInt(level);
     /* edit display logic here */
     var suffix = 1;
-    if (level < 25) {
-        suffix = 1;
-    } else if (level < 50) {
-        suffix = 2;
-    } else if (level < 75) {
+    if (level < 19) {
         suffix = 3;
+    } else if (scen == '15') {
+        suffix = 2;
+    } else if (scen == '16') {
+        suffix = 1;
     } else {
-        suffix = 4;
+        suffix = 1;
     }
     /* end edit */
 
-    d3.select('.dam').style('display', 'none');
     var dam_id = '#' + dam_name + '-' + suffix;
     var node = container.select(dam_id).style('display', 'block');
     container.select('#' + dam_name + '-text').text(level + '%');
@@ -24,7 +24,7 @@ function display_overlay(dam_name, level) {
 
 queue()
     .defer(d3.xml, 'dams.svg')
-    .defer(d3.csv, 'data.csv')
+    .defer(d3.csv, 'data_wc.csv')
     .await(function(error, xml, csv) {
         // DON'T NEED TO EDIT ANYTHING HERE
         /* Insert graphic into html page */
